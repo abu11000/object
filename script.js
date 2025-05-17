d0f36a2f489bacf82a3535931aba84afe6146c8a
abu11000 <1337legend2000@gmail.com>
2025-05-17 14:05:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let a = prompt('write your id')
+let b = prompt('write your name')
+let c = prompt('write your password')
+const data = { id: a, name: b, passsword: c }
+
+
+console.log(data);
+
